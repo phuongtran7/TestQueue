430cff37122808200f62c052f2d56331b10bf6b2
phuongtran7 <phuong.tran@jvncomm.com>
2020-02-18 20:02:13 -0500

    clean up

--- TestQueue/TestQueue.cpp
+++ TestQueue/TestQueue.cpp
@@ -1,20 +1,16 @@
-// TestQueue.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
 #include "readerwriterqueue/readerwriterqueue.h"
 #include "fmt/format.h"
-#include <vector>
 #include <thread>
+#include <atomic>
 
 auto main() -> int {
-	std::cout << "Hello World!\n";
-
 	moodycamel::ReaderWriterQueue<int> q(1);
+
+	std::atomic_bool running { true };
 
 	std::thread t1([&] {
 		auto count = 0;
-		while (1) {
+		while (running) {
 			q.try_enqueue(count);
 			count++;
 			std::this_thread::sleep_for(std::chrono::milliseconds(5));
@@ -22,7 +18,7 @@ auto main() -> int {
 		});
 
 	std::thread t2([&] {
-		while (1) {
+		while (running) {
 			int result{};
 			if (q.try_dequeue(result)) {
 				fmt::print("{}\n", result);
@@ -31,6 +27,8 @@ auto main() -> int {
 		}
 		});
 
+	std::getchar();
+	running.store(false);
 	t1.join();
 	t2.join();
 	return 0;
